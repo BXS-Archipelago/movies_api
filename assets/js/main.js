86641081d18e7704ba6f85e4f3db0fb55c6be1dc
BXS-Archipelago <bxs@tutanota.com>
2022-09-23 16:58:30 +0000

    create hero section

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+'use strict'
+console.log('hello')
+
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+
+  navElemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("active");
+
+  });
+
+}
+
+
+// header sticky
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function() {
+    this.window.scrollY >= 10 ? header.classList.add("active"): header.classList.remove("active");
+});
